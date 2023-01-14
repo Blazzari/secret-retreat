30cc0887f94b39b4ee8af63b03f4515fbb1ddf09
Lazzari <baptiste.lazzari@outlook.fr>
2023-01-14 15:49:41 +0100

    ajout des models et routes pour rooms et bookings ajout model user via devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :rooms do
+    resources :bookings, only: %i[create new]
+  end
+  resources :bookings, only: %i[edit update destroy show]
 end
