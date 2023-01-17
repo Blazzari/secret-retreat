361775f8e1b54192e2148b2eed4cf66a2ab6b5ec
Lazzari <baptiste.lazzari@outlook.fr>
2023-01-17 20:22:07 +0100

    ajout de la seed user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts "new seed launched, destroying old users !"
+
+User.destroy_all
+
+puts "create 3 new users !"
+
+User.create!({email: "user1@mail.com", password: "u1u1u1" })
+User.create!({email: "user2@mail.com", password: "u2u2u2" })
+User.create!({email: "user3@mail.com", password: "u3u3u3" })
+
+puts "3 new users created !"
+
+# create_table "users", force: :cascade do |t|
+#   t.string "email", default: "", null: false
+#   t.string "encrypted_password", default: "", null: false
+#   t.string "reset_password_token"
+#   t.datetime "reset_password_sent_at"
+#   t.datetime "remember_created_at"
+#   t.datetime "created_at", null: false
+#   t.datetime "updated_at", null: false
+#   t.index ["email"], name: "index_users_on_email", unique: true
+#   t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+# end
