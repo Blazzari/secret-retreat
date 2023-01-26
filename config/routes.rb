103f1cb9340ee22bb091976d77029eefdcd05ed1
Lazzari <baptiste.lazzari@outlook.fr>
2023-01-26 21:34:34 +0100

    rooms my rooms

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  get 'reviews/new'
-  get 'reviews/create'
   devise_for :users
   root to: "pages#home"
-
+  get "my_rooms", to: "rooms#my_rooms"
   resources :rooms do
     resources :bookings, only: %i[create new]
     resources :reviews, only: %i[create new]
