5e35028d95882bc71e54f72095cf5b82e50b7418
Lazzari <baptiste.lazzari@outlook.fr>
2023-01-27 02:52:02 +0100

    plein de trucs

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,5 @@
 class Booking < ApplicationRecord
   belongs_to :room, dependent: :destroy
   belongs_to :user, dependent: :destroy
+  validates :end_date, comparison: { greater_than: :start_date }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   get "my_rooms", to: "rooms#my_rooms"
+  get "dashboard", to: "bookings#dashboard"
+  get "dashboard_booked", to: "bookings#dashboard_booked"
   resources :rooms do
     resources :bookings, only: %i[create new]
     resources :reviews, only: %i[create new]
